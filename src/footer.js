3d1c9c50914435db3ca153607450364ec7808d39
mayaclaros <mayawclaros@gmail.com>
2024-12-08 21:11:03 -0500

    Footer.js
    Created footer component

---
+++ src/footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        This project was coded with 💜 by
+        <a
+          href="https://www.justmeomaya.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Maya Claros
+        </a>
+        and is open sourced on
+        <a
+          href="https://github.com/mayaclaros/mc-lyte"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub
+        </a>
+        and hosted by
+        <a
+          href="https://mclyte.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Netlify
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
